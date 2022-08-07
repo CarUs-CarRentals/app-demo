eb74acfe89390d237c0f0b32b59d49ef4b332cea
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-08-06 23:17:20 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/carshare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carshare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  static const String _title = '';
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData myTheme = ThemeData();
+
+    return MaterialApp(
+      title: _title,
+      home: const MyHomePage(),
+      theme: myTheme.copyWith(
+        colorScheme: myTheme.colorScheme.copyWith(
+          primary: Colors.deepPurple,
+          secondary: Colors.grey[700],
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Minhas Viagens',
+      style: optionStyle,
+    ),
+    Text(
+      'Conta',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.speed),
+            label: 'Minhas Viagens',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Conta',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Theme.of(context).colorScheme.secondary,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
