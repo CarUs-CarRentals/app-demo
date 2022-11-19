71d46354e51b463e33831e9e49cfa44b8d7a08e2
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-11-19 18:46:40 -0300

    creating mask formatters class

---
+++ lib/components/maskFormatters.dart
@@ -0,0 +1,17 @@
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class FieldTextMask {
+  static MaskTextInputFormatter get maskPhoneNumber {
+    return MaskTextInputFormatter(
+        mask: '+## (##) #####-####',
+        filter: {"#": RegExp(r'[0-9]')},
+        type: MaskAutoCompletionType.lazy);
+  }
+
+  static MaskTextInputFormatter get maskCPF {
+    return MaskTextInputFormatter(
+        mask: '###.###.###-##',
+        filter: {"#": RegExp(r'[0-9]')},
+        type: MaskAutoCompletionType.lazy);
+  }
+}
