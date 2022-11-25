82cf3bb17d661d80a702f0d4137155964c890dcd
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-11-24 22:55:14 -0300

    removing dummy data

--- lib/data/dummy_users_review_data.dart
+++ lib/data/dummy_users_review_data.dart
@@ -1,63 +1,63 @@
 import '../models/review.dart';
 
 List<UserReview> dummyUsersReviews = [
-  UserReview(
-    id: '1',
-    userIdRated: 'P9O0G1Ul8BRixV6akx9uNLb5H6A2',
-    userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
-    description: 'Muito bom',
-    rate: 1.0,
-    date: DateTime.now().add(Duration(days: -5)),
-  ),
-  UserReview(
-    id: '2',
-    userIdRated: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
-    userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
-    description:
-        'bom, mas o cinto de segurança é traseiro curto, incompatível com cadeirinha infantil e o porta-malas é pequeno',
-    rate: 4,
-    date: DateTime.now().add(Duration(days: -1)),
-  ),
-  UserReview(
-    id: '3',
-    userIdRated: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
-    userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
-    description: 'Já vi melhores',
-    rate: 3.5,
-    date: DateTime.now(),
-  ),
-  UserReview(
-    id: '4',
-    userIdRated: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
-    userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
-    description: 'Tava bom, aí melhorou, agora parece que está pior',
-    rate: 3,
-    date: DateTime.now(),
-  ),
-  UserReview(
-    id: '5',
-    userIdRated: 'P9O0G1Ul8BRixV6akx9uNLb5H6A2',
-    userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
-    description: 'Maravilindo!!! ❤❤❤ Adorei.',
-    rate: 5,
-    date: DateTime.now(),
-  ),
-  UserReview(
-    id: '6',
-    userIdRated: 'yiKoZG8TSUhv1fd7W572vAn46M02',
-    userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
-    description:
-        'Trouxe o carro com tanque cheio conforme entreguei, muito bom!!\n\nUm verdadeiro Cheiroso 😍',
-    rate: 4,
-    date: DateTime.now(),
-  ),
-  UserReview(
-    id: '7',
-    userIdRated: 'yiKoZG8TSUhv1fd7W572vAn46M02',
-    userIdEvaluator: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
-    description:
-        'O Igor é um baita macho, pode emprestar o carro pra ele é de confiaça\nEu agarantio',
-    rate: 5,
-    date: DateTime.now().add(Duration(days: -8)),
-  ),
+  // UserReview(
+  //   id: '1',
+  //   userIdRated: 'P9O0G1Ul8BRixV6akx9uNLb5H6A2',
+  //   userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
+  //   description: 'Muito bom',
+  //   rate: 1.0,
+  //   date: DateTime.now().add(Duration(days: -5)),
+  // ),
+  // UserReview(
+  //   id: '2',
+  //   userIdRated: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
+  //   userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
+  //   description:
+  //       'bom, mas o cinto de segurança é traseiro curto, incompatível com cadeirinha infantil e o porta-malas é pequeno',
+  //   rate: 4,
+  //   date: DateTime.now().add(Duration(days: -1)),
+  // ),
+  // UserReview(
+  //   id: '3',
+  //   userIdRated: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
+  //   userIdEvaluator: 'yiKoZG8TSUhv1fd7W572vAn46M02',
+  //   description: 'Já vi melhores',
+  //   rate: 3.5,
+  //   date: DateTime.now(),
+  // ),
+  // UserReview(
+  //   id: '4',
+  //   userIdRated: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
+  //   userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
+  //   description: 'Tava bom, aí melhorou, agora parece que está pior',
+  //   rate: 3,
+  //   date: DateTime.now(),
+  // ),
+  // UserReview(
+  //   id: '5',
+  //   userIdRated: 'P9O0G1Ul8BRixV6akx9uNLb5H6A2',
+  //   userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
+  //   description: 'Maravilindo!!! ❤❤❤ Adorei.',
+  //   rate: 5,
+  //   date: DateTime.now(),
+  // ),
+  // UserReview(
+  //   id: '6',
+  //   userIdRated: 'yiKoZG8TSUhv1fd7W572vAn46M02',
+  //   userIdEvaluator: 'TgLAhXEwFndGYCoucrbdpafGc2k1',
+  //   description:
+  //       'Trouxe o carro com tanque cheio conforme entreguei, muito bom!!\n\nUm verdadeiro Cheiroso 😍',
+  //   rate: 4,
+  //   date: DateTime.now(),
+  // ),
+  // UserReview(
+  //   id: '7',
+  //   userIdRated: 'yiKoZG8TSUhv1fd7W572vAn46M02',
+  //   userIdEvaluator: 'lRndMwYM46PRVRmSxlJo1dJxwJb2',
+  //   description:
+  //       'O Igor é um baita macho, pode emprestar o carro pra ele é de confiaça\nEu agarantio',
+  //   rate: 5,
+  //   date: DateTime.now().add(Duration(days: -8)),
+  // ),
 ];
